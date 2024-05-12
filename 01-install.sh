98fe0f55f930ed775ab62098a207b06dd9313028
Jiho Lee <optional.int@kakao.com>
2024-05-12 12:50:41 +0900

    feat: Initial commit

---
+++ 01-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Enable debug output
+PS4="\n\033[1;33m>>\033[0m "; set -x
+
+LOCATION=$(realpath "$0")
+DIR=$(dirname "$LOCATION")
+
+curl -sfL https://get.k3s.io | sh -s - server --cluster-init
