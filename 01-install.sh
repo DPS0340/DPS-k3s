b1ac445765b89920a47897fcf2b8d76a00b3dbee
Jiho Lee <optional.int@kakao.com>
2024-08-04 16:46:59 +0900

    feat: set api-server listen port to 26444

--- 01-install.sh
+++ 01-install.sh
@@ -6,6 +6,6 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 LOCATION=$(realpath "$0")
 DIR=$(dirname "$LOCATION")
 
-curl -sfL https://get.k3s.io | sh -s - server --cluster-init
+curl -sfL https://get.k3s.io | sh -s - server --https-listen-port=26444 --cluster-init
 
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
